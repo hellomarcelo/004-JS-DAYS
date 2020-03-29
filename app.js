1f34249e69f2cef01e1db0cda171c4294b2ff47f
Marcelo Rodriguez <39660702+hellomarcelo@users.noreply.github.com>
2020-03-28 21:19:08 -0300

    Update app.js
    app

--- app.js
+++ app.js
@@ -3,50 +3,50 @@ let day = new Date();
 
 switch(day.getDay()) {
     case 0:
-         let day = document.getElementById('day');
-        day.innerHTML = ` 
+         let day0 = document.getElementById('day');
+        day0.innerHTML = ` 
         <h1>Domingo</h1>
         `
     break;
 
     case 1:
-     let day = document.getElementById('day');
-        day.innerHTML = ` 
+     let day1 = document.getElementById('day');
+        day1.innerHTML = ` 
         <h1>Lunes</h1>
         `
     break;
 
     case 2:
-     let day = document.getElementById('day');
-        day.innerHTML = ` 
+     let day2 = document.getElementById('day');
+        day2.innerHTML = ` 
         <h1>Martes</h1>
         `
     break;
 
     case 3:
-    let day = document.getElementById('day');
-        day.innerHTML = ` 
+    let day3 = document.getElementById('day');
+        day3.innerHTML = ` 
         <h1>Míercoles</h1>
         `
     break;
 
     case 4:
-    let day = document.getElementById('day');
-        day.innerHTML = ` 
+    let day4 = document.getElementById('day');
+        day4.innerHTML = ` 
         <h1>Jueves</h1>
         `
     break;
 
     case 5:
-     let day = document.getElementById('day');
-        day.innerHTML = ` 
+     let day5 = document.getElementById('day');
+        day5.innerHTML = ` 
         <h1>Viernes</h1>
         `
     break;
 
     case 6:
-        let day = document.getElementById('day');
-        day.innerHTML = ` 
+        let day6 = document.getElementById('day');
+        day6.innerHTML = ` 
         <h1>Sábado</h1>
         `
     break;
